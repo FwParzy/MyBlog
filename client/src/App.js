933fbef0ef098783119f919a80e4f14353aad4c7
FwParzy <guardianiisaac@gmail.com>
2023-07-02 17:01:59 -0400

    formatted_api_and_fixed_tabstop_client

--- client/src/App.js
+++ client/src/App.js
@@ -9,52 +9,52 @@ import Footer from './components/Footer';
 import './style.scss';
 
 const Layout = () => {
-    return (
-        <>
-            <Navbar />
-            <Outlet />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 const router = createBrowserRouter([
-    {
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
         path: '/',
-        element: <Layout />,
-        children: [
-            {
-                path: '/',
-                element: <Home />,
-            },
-            {
-                path: '/post/:id',
-                element: <Single />,
-            },
-            {
-                path: '/write',
-                element: <Write />,
-            },
-        ],
-    },
-    {
-        path: '/register',
-        element: <Register />,
-    },
-    {
-        path: '/login',
-        element: <Login />,
-    },
+        element: <Home />,
+      },
+      {
+        path: '/post/:id',
+        element: <Single />,
+      },
+      {
+        path: '/write',
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
 ]);
 
 function App() {
-    return (
-        <div className="app">
-            <div className="container">
-                <RouterProvider router={router} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
