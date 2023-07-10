9f8400268b32be9b3296a4fb4faefa5a984aaffa
FwParzy <guardianiisaac@gmail.com>
2023-07-10 16:06:36 -0400

    removing_comments

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,32 +18,6 @@ const Home = () => {
     };
     fetchData();
   }, [cat]);
-  // const posts = [
-  //   {
-  //     id: 1,
-  //     title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-  //     desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-  //     img: 'https://emsnest.org/wp-content/uploads/2023/01/avatars-ylM4tanwBXJZhjWb-X76mWw-t500x500.jpg',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-  //     desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-  //     img: 'https://o.aolcdn.com/hss/storage/midas/832ed1bdd3c2e2279112403d8bfb3e80/200374623/snow+monkey+iphone.jpg',
-  //   },
-  //   {
-  //     id: 3,
-  //     title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-  //     desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-  //     img: 'https://t4.ftcdn.net/jpg/01/44/89/37/360_F_144893797_GyALGMXV207yrF9YMczEEXFwKYOYIfSP.jpg',
-  //   },
-  //   {
-  //     id: 4,
-  //     title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-  //     desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-  //     img: 'https://images.csmonitor.com/csmarchives/2010/12/1203-monkeys-emperor-tamarin.jpg?alias=standard_900x600nc',
-  //   },
-  // ];
 
   return (
     <div className="home">
