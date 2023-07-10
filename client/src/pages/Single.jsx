5be316cc301797c0290aab87767fa8b7b319161a
FwParzy <guardianiisaac@gmail.com>
2023-07-10 15:56:24 -0400

    fetching_sql

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,75 +1,68 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Menu from '../components/Menu';
 import Edit from '../img/edit.png';
 import Delete from '../img/delete.png';
 import User from '../img/monke_suit.jpg';
-import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/authContext';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import moment from 'moment';
 
 const Single = () => {
+  const [post, setPost] = useState([]);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const postId = location.pathname.split('/')[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="single">
       <div className="content">
-        <img
-          src="http://www.todayifoundout.com/wp-content/uploads/2015/11/chimp-on-a-laptop.png"
-          alt=""
-        />
+        <img src={post?.postImg} alt="" />
         <div className="user">
-          <img src={User} alt="" />
+          {post.userImg && <img src={post.userImg} alt="" />}
+          {/* <img src={User} alt="" /> */}
           <div className="info">
-            <span>Jon</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>{moment(post.date).fromNow()}</p>
           </div>
-          <div className="edit">
-            {/*TODO Change 2 to post number */}
-            <Link to={'/write?edit=2'}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img src={Delete} alt="" />
-          </div>
+          {currentUser.username &&
+            post.username &&
+            currentUser.username === post.username && (
+              <div className="edit">
+                <Link to={'/write?edit=6'}>
+                  <img src={Edit} alt="" />
+                </Link>
+                <img onClick={handleDelete} src={Delete} alt="" />
+              </div>
+            )}
         </div>
-        <h1>
-          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-          cillum sint consectetur cupidatat.
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
-          enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
-          exercitation amet. Nisi anim cupidatat excepteur officia.
-          Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
-          voluptate dolor minim nulla est proident. Nostrud officia pariatur ut
-          officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit
-          commodo officia dolor Lorem duis laboris cupidatat officia voluptate.
-          Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis
-          officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis
-          sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea
-          consectetur et est culpa et culpa duis.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
-          enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
-          exercitation amet. Nisi anim cupidatat excepteur officia.
-          Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
-          voluptate dolor minim nulla est proident. Nostrud officia pariatur ut
-          officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit
-          commodo officia dolor Lorem duis laboris cupidatat officia voluptate.
-          Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis
-          officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis
-          sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea
-          consectetur et est culpa et culpa duis.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
-          enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
-          exercitation amet. Nisi anim cupidatat excepteur officia.
-          Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
-          voluptate dolor minim nulla est proident. Nostrud officia pariatur ut
-          officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit
-          commodo officia dolor Lorem duis laboris cupidatat officia voluptate.
-          Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis
-          officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis
-          sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea
-          consectetur et est culpa et culpa duis.
-        </p>
+        <h1>{post.title}</h1>
+        {post.descr}
       </div>
       <Menu />
       <div className="menue"></div>
